7598144f7650a07f9c240f8fa2c78d74d1800572
QingTian <qingtian63910@proton.me>
2024-05-19 13:57:17 +0700

    feat: updated site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+    output: "hybrid",
+    adapter: node({
+        mode: "standalone"
+    }),
+
+    server: {
+        host: true,
+        port: 6969,
+    },
+
+    i18n: {
+        defaultLocale: "vi",
+    },
+
+    integrations: [
+        tailwind(),
+        react()
+    ],
 });
