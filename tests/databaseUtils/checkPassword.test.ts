6e67173566fdada369762c799454c99b3327cb64
QingTian <qingtian63910@proton.me>
2024-05-20 19:30:07 +0700

    feat: added an unit test for checkPassword().

---
+++ tests/databaseUtils/checkPassword.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "vitest";
+import { checkPassword } from "../../src/database/databaseUtils";
+
+/* Minimum password requirements:
+ *
+ *   *) At least 6 characters in length
+ *   *) At least 1 UPPERCASE character
+ *   *) At least 1 lowercase character
+ *   *) At least 1 digit (0-9)
+ */
+
+test("'Password123' -> checkPassword() -> true", () => {
+    expect(checkPassword("Password123")).toBe(true);
+})
