83677f7dad0494137b53e07c9f0454c2a1a7ace0
QingTian <qingtian63910@proton.me>
2024-05-23 11:48:27 +0700

    feat: created Color enum table.

---
+++ supabase/migrations/20240522033222_create_color_enum.sql
@@ -0,0 +1,13 @@
+create type "Color" as enum (
+    'black',
+    'white',
+    'blue',
+    'red',
+    'green',
+    'yellow',
+    'orange',
+    'purple',
+    'pink',
+    'brown',
+    'gray'
+);
