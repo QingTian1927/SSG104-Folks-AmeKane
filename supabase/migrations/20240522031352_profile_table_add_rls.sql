df7174daec21ec150190bdaf99f4ddff7af47fdd
QingTian <qingtian63910@proton.me>
2024-05-23 11:47:57 +0700

    refactor: squashed some db migrations

--- supabase/migrations/20240522031352_profile_table_add_rls.sql
+++ supabase/migrations/20240522031352_profile_table_add_rls.sql
@@ -10,6 +10,8 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
+
 CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
 
 COMMENT ON SCHEMA "public" IS 'standard public schema';
@@ -26,12 +28,40 @@ CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."Profile" (
+    "user_id" "uuid" NOT NULL,
+    "display_name" "text",
+    "default_account" "uuid",
+    "currency" character varying DEFAULT 'đ'::character varying,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL
+);
+
+ALTER TABLE "public"."Profile" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."Profile"
+    ADD CONSTRAINT "profile_pkey" PRIMARY KEY ("user_id");
+
+ALTER TABLE ONLY "public"."Profile"
+    ADD CONSTRAINT "profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE POLICY "Users can only view their own profile" ON "public"."Profile" FOR SELECT USING ((( SELECT "auth"."uid"() AS "uid") = "user_id"));
+
+ALTER TABLE "public"."Profile" ENABLE ROW LEVEL SECURITY;
+
 ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
 
 GRANT USAGE ON SCHEMA "public" TO "postgres";
 GRANT USAGE ON SCHEMA "public" TO "anon";
 GRANT USAGE ON SCHEMA "public" TO "authenticated";
 GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."Profile" TO "anon";
+GRANT ALL ON TABLE "public"."Profile" TO "authenticated";
+GRANT ALL ON TABLE "public"."Profile" TO "service_role";
 
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
@@ -49,3 +79,8 @@ ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TAB
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
 
 RESET ALL;
+
+--
+-- Dumped schema changes for auth and storage
+--
+
