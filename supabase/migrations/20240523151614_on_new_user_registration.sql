dedacb252d845f36a907ae8cd67c6be62ae36382
QingTian <qingtian63910@proton.me>
2024-05-28 22:37:21 +0700

    refactor: renamed a table.

--- supabase/migrations/20240523151614_on_new_user_registration.sql
+++ supabase/migrations/20240523151614_on_new_user_registration.sql
@@ -4,12 +4,12 @@ language plpgsql
 security definer set search_path = ''
 as $$
 begin
-    insert into public."Profile" (user_id) values (new.id);
+    insert into public."Preferences" (user_id) values (new.id);
 
     return new;
 end;
 $$;
 
-create trigger on_auth_user_created
+create or replace trigger on_auth_user_created
     after insert on auth.users
     for each row execute procedure public.handle_new_user();
