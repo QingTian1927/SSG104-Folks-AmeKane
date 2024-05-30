c19ff404cf4f7201a6a3d6d2f2940d2614175064
QingTian <qingtian63910@proton.me>
2024-05-30 17:09:36 +0700

    feat: added types to some supabase get functions.
    * Added some basic type-checking to functions that fetch data from supabase.

--- src/database/supabaseClient.ts
+++ src/database/supabaseClient.ts
@@ -11,18 +11,38 @@ export const supabase = createClient<Database>(
     },
 );
 
-export async function getAccount() {
-    return await supabase.from("Account").select();
+export type UserId = string | undefined;
+
+const UndefinedUserIdError = (userId: any) => `User ID is undefined: "${userId}"`;
+
+export async function getAccount(userId: UserId) {
+    if (!userId) {
+        return { data: [], error: UndefinedUserIdError(userId) }
+    }
+    return await supabase.from("Account").select().eq('user_id', userId);
 }
-export async function getPreferences() {
-    return await supabase.from("Preferences").select();
+
+export async function getPreferences(userId: UserId) {
+    if (!userId) {
+        return { data: [], error: UndefinedUserIdError(userId) }
+    }
+    return await supabase.from("Preferences").select().eq('user_id', userId);
 }
-export async function getCategory() {
-    return await supabase.from("Category").select();
+
+export async function getCategory(userId: UserId) {
+    if (!userId) {
+        return { data: [], error: UndefinedUserIdError(userId) }
+    }
+    return await supabase.from("Category").select().eq('user_id', userId);
 }
-export async function getGoal() {
-    return await supabase.from("Goal").select();
+
+export async function getGoal(userId: UserId) {
+    if (!userId) {
+        return { data: [], error: UndefinedUserIdError(userId) }
+    }
+    return await supabase.from("Goal").select().eq('user_id', userId);
 }
+
 export async function getTransaction() {
     return await supabase.from("Transaction").select();
 }
