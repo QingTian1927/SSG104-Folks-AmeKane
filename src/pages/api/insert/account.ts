172967b4e64f31304544386498fef35620438cee
QingTian <qingtian63910@proton.me>
2024-06-01 15:59:21 +0700

    feat: added an api route to create a new account.
    * Added an API route to insert a new (financial) account into the Account table.

---
+++ src/pages/api/insert/account.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+import { db } from "../../../database/databaseUtils";
+import { toBoolean, toNumber } from "../../../database/typeConversion";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    const formData = await request.formData();
+    const title = formData.get("title")?.toString();
+    const balance = formData.get("balance")?.toString();
+    const is_saving = formData.get("is_saving")?.toString();
+
+    if (!title) {
+        return new Response(
+            "The account title is required", { status: 400 }
+        );
+    }
+
+    const userId = await db.auth.getUserId();
+    if (!userId) {
+        return new Response(
+            "Could not retrieve the User ID", { status: 500 }
+        );
+    }
+
+    const { error } = await db.insert.account({
+        user_id: userId,
+        title: title,
+        balance: toNumber(balance),
+        is_saving: toBoolean(is_saving),
+    });
+
+    if (error) {
+        return new Response(
+            "Could not insert the newly created account\n" + error.message,
+            { status: 500 }
+        );
+    }
+
+    return redirect("/dashboard");
+}
