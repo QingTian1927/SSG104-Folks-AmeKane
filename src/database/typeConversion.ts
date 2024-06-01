8233fd95a588596227652bd49c46af9adf796fbc
QingTian <qingtian63910@proton.me>
2024-06-01 15:58:22 +0700

    feat: added some functions to convert data types.
    * Added some functions to convert string into boolean or number.

---
+++ src/database/typeConversion.ts
@@ -0,0 +1,18 @@
+export function toBoolean(value?: string | number | boolean): boolean {
+    if (!value) {
+        return false;
+    }
+    const truthy = [true, "true", "1", 1, "yes", "y"];
+
+    if (typeof value === "string") {
+        return truthy.includes(value.toLocaleLowerCase());
+    }
+    return truthy.includes(value);
+}
+
+export function toNumber(value?: string): number {
+    if (!value) {
+        return Number.NaN;
+    }
+    return Number.parseFloat(value);
+}
