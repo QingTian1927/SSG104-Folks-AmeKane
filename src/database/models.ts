03807ca7eca4190011c2a86dd0909140874b6dfd
QingTian <qingtian63910@proton.me>
2024-06-04 08:44:12 +0700

    feat: added extra error messages.

--- src/database/models.ts
+++ src/database/models.ts
@@ -5,7 +5,8 @@ export const ERROR_MESSAGES = {
     UNDEFINED_ACCOUNT_ID: "Account ID is undefined",
     UNDEFINED_CATEGORY_ID: "Category ID is undefined",
     INVALID_TRANSACTION_VALUE: "Transaction value must be greater than 0",
-    INVALID_ACCOUNT_BALANCE: "Account balance must be greater than 0",
+    INVALID_ACCOUNT_BALANCE: "Account balance must be a real number",
+    INVALID_CATEGORY_SPENDING_LIMIT: "Category spending limit must be greater or equal to zero"
 } as const;
 
 export function errorResponse(param: any, message: string) {
