ea82bb1d24368efcaf9c6815f9012eca89b644e7
QingTian <qingtian63910@proton.me>
2024-06-04 15:33:11 +0700

    feat: added an error message for invalid transaction values.

--- src/database/models.ts
+++ src/database/models.ts
@@ -4,6 +4,8 @@ export const ERROR_MESSAGES = {
     UNDEFINED_USER_ID: "User ID is undefined",
     UNDEFINED_ACCOUNT_ID: "Account ID is undefined",
     UNDEFINED_CATEGORY_ID: "Category ID is undefined",
+    UNDEFINED_TRANSACTION_ID: "Transaction ID is undefined",
+
     INVALID_TRANSACTION_VALUE: "Transaction value must be greater than 0",
     INVALID_ACCOUNT_BALANCE: "Account balance must be a real number",
     INVALID_CATEGORY_SPENDING_LIMIT: "Category spending limit must be greater or equal to zero",
