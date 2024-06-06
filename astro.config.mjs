0d1e4bf473f7f5211b78b1f95b72641c209f2f38
QingTian <qingtian63910@proton.me>
2024-06-06 11:26:39 +0700

    chore: added astro sitemap integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,29 @@
-import { defineConfig } from 'astro/config';
-import { loadEnv } from 'vite';
-
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
     site: SITE_URL,
-
     output: "server",
     adapter: node({
-        mode: "standalone"
+        mode: "standalone",
     }),
-
     build: {
         server: "./dist/server",
         serverEntry: "entry.mjs",
     },
-
     server: {
         host: true,
         port: 6969,
     },
-
     i18n: {
         defaultLocale: "vi",
-        locales: [
-            "vi",
-        ]
+        locales: ["vi"],
     },
-
-    integrations: [
-        tailwind(),
-        react()
-    ],
+    integrations: [tailwind(), react(), sitemap()],
 });
