ace371fafd09e6b30283aa165b5781f2f29aafd6
QingTian <qingtian63910@proton.me>
2024-06-08 10:44:29 +0700

    feat: added Montserrat from Google Fonts.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                "sans": ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+            },
+        },
 	},
 	plugins: [
         require("@tailwindcss/typography"),
