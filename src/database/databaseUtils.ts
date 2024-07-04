4b367ea1f5f798c49536e666f1a496237d00943a
QingTian <qingtian63910@proton.me>
2024-07-04 11:33:36 +0700

    feat: created a function to calculate total saving.
    * Created a postgres function to calculate the total amount of saving based on goals' current progress.

--- src/database/databaseUtils.ts
+++ src/database/databaseUtils.ts
@@ -27,6 +27,7 @@ export const db = {
         getTotalBalance: supaUtils.getTotalBalance,
         getTotalExpense: supaUtils.getTotalExpense,
         getTotalIncome: supaUtils.getTotalIncome,
+        getTotalSaving: supaUtils.getTotalSaving,
         getTotalSpendingByCategory: supaUtils.getTotalSpendingByCategory,
         getGoalProgress: supaUtils.getGoalProgress,
     },
