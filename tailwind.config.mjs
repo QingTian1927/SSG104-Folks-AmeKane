5ff54528676c2363b624edf443920a850e027a0a
QingTian <qingtian63910@proton.me>
2024-07-13 15:38:44 +0700

    refactor: tailwind dark mode selector toggle.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
+    darkMode: "selector",
+
     theme: {
         extend: {
             fontFamily: {
