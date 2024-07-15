cfa9332bbc995e0350b487ba887a51214fbfffe7
QingTian <qingtian63910@proton.me>
2024-07-15 22:39:18 +0700

    refactor: delete some cookies on sign out.

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -4,5 +4,8 @@ export const GET: APIRoute = async ({ cookies, redirect }) => {
     cookies.delete("sb-access-token", { path: "/" });
     cookies.delete("sb-refresh-token", { path: "/" });
 
+    cookies.delete("login-error", { path: "/signin "});
+    cookies.delete("theme", { path: "/" });
+
     return redirect("/signin");
 };
